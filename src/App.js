5638c71daa47f53fcf48ab9601edbdcd4b1c3256
Nealjai <brayant414@gmail.com>
2025-03-23 00:24:32 +0800

    First Edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import CardList from "./components/CardList";
-import SearchBox from "./SearchBox";
+import SearchBox from "./components/SearchBox";
 import { robots } from "./robots";
 
 
